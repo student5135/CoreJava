3fc60e6a6ffe62ccd1a40de90ffe3747e13b91fa
student5135 <adhavrohan5135@gmail.com>
2025-02-15 22:06:37 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+class evenodd
+{
+	public static void main(String[] args)
+	{
+		int num=11;
+		if(num%2==0)
+		{
+			System.out.println(num+" IS EVEN NUMBER");
+
+		}
+		else 
+		{
+			System.out.println(num+" IS ODD NUMBER");
+		}
+	}
+}
