f46fb37aa986f7816a21acbfddba5d9bcfcd9ecc
student5135 <adhavrohan5135@gmail.com>
2025-02-23 10:48:05 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,27 @@
+class Prime
+{
+	public static void main(String[] args)
+	{
+		int num=5;
+		int count=0;
+		for(int i=1;i<=num;i++)
+         {
+          if(num%i==0)
+          {
+          	count++;
+          }
+          }
+          if(count==2)
+         {
+          	System.out.println(num+" is prime number");
+          }
+          else 
+          {
+          	System.out.println(num+" is  not prime number");
+          }
+              
+
+
+         }
+	}
+
