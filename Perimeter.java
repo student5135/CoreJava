8c0fb142570723fa476f7ee6dc3e9697c36a2765
Rohan Adhav <adhavrohan5135@gmail.com>
2025-02-27 11:57:55 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,10 @@
+class Perimeter{
+	public static void main(String[] args){
+		double pie=3.14d;
+		double radius=21.1d;
+
+		double perimeterofcircle=(2*pie*radius);
+
+		System.out.println("Perimeter of Circle IS :" +perimeterofcircle);
+	}
+}
