f50740dbba62ef7543283d1668a005c7c19e4586
Rohan Adhav <adhavrohan5135@gmail.com>
2025-03-26 11:08:54 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,11 @@
+class LeapYear {
+    public static void main(String[] args) {
+        int year = 2024; // You can change the year manually
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
