22df3b47370ca29c19ed71d70f70515c95443a2c
Rohan Adhav <adhavrohan5135@gmail.com>
2025-04-01 16:33:01 +0530

    Create SquareSeries.java

---
+++ SquareSeries.java
@@ -0,0 +1,10 @@
+public class SquareSeries {                      //creating a class
+    public static void main(String[] args) {                //main method
+        int n = 10;                                        //declaring and initializing n variable
+        
+        System.out.print("Square Number Series: ");                              //printing the statement
+        for (int i = 1; i <= n; i++) {                                             //using for loop
+            System.out.print((i * i) + " ");
+        }
+    }
+}
